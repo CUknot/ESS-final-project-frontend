4bee5ae07091b65f80312be49d603ec80b83c5e4
Kittipat Jusingha <kknot2004@gmail.com>
2023-04-29 23:13:14 +0700

    first commit

--- script_cv.js
+++ script_cv.js
@@ -7,7 +7,7 @@ const authorizeApplication = () => {
 
 // TODO #3.1: Change group number
 const getGroupNumber = () => {
-  return 99;
+  return 13;
 };
 
 // Example: Send Get user profile ("GET") request to backend server and show the response on the webpage
@@ -36,10 +36,27 @@ const getUserProfile = async () => {
 // TODO #3.3: Send Get Courses ("GET") request to backend server and filter the response to get Comp Eng Ess CV_cid
 //            and display the result on the webpage
 const getCompEngEssCid = async () => {
-  document.getElementById("ces-cid-value").innerHTML = "";
-  console.log(
-    "This function should fetch 'get courses' route from backend server and find cv_cid value of Comp Eng Ess."
-  );
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+  await fetch(
+    `http://${backendIPAddress}/courseville/get_courses`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => data.data.student)
+    .then((course) => {
+      for (let i = 0; i < course.length; i++) {
+        if (course[i].course_no == "2110221") {
+          document.getElementById("ces-cid-value").innerHTML = course[i].cv_cid;
+          break;
+        }
+      }
+    })
+    .catch((error) => console.error(error));
+
+    //document.getElementById("ces-cid-value").innerHTML = "";
 };
 
 // TODO #3.5: Send Get Course Assignments ("GET") request with cv_cid to backend server
@@ -49,9 +66,27 @@ const createCompEngEssAssignmentTable = async () => {
   table_body.innerHTML = "";
   const cv_cid = document.getElementById("ces-cid-value").innerHTML;
 
-  console.log(
-    "This function should fetch 'get course assignments' route from backend server and show assignments in the table."
-  );
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+  await fetch(
+    `http://${backendIPAddress}/courseville/get_course_assignments/${cv_cid}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      items = data.data})
+    .catch((error) => console.error(error));
+  items.map((item) => {
+    table_body.innerHTML += `
+      <tr id="${item.itemid}">
+        <td>${item.itemid}</td>
+        <td>${item.title}</td>
+      </tr>
+    `;
+  })
 };
 
 const logout = async () => {
--- script_item.js
+++ script_item.js
@@ -5,11 +5,11 @@ let itemsData;
 
 // TODO #2.1: Edit group number
 const getGroupNumber = () => {
-  return 99;
+  return 13;
 };
 
 // TODO #2.2: Show group members
-const showGroupMembers = async () => {
+/*const showGroupMembers = async () => {
   const member_list = document.getElementById("member-list");
   member_list.innerHTML = "";
   const member_dropdown = document.getElementById("name-to-add");
@@ -33,52 +33,191 @@ const showGroupMembers = async () => {
       });
     })
     .catch((error) => console.error(error));
-};
+};*/
 
 // TODO #2.3: Send Get items ("GET") request to backend server and store the response in itemsData variable
 const getItemsFromDB = async () => {
-  console.log(
-    "This function should fetch 'get items' route from backend server."
-  );
+  const options = {
+    method: "GET",
+    credentials: "include",
+  };
+  await fetch(`http://${backendIPAddress}/items`, options)
+    .then((response) => response.json())
+    .then((data) => {
+      itemsData = data;
+    })
+    .catch((error) => console.error(error));
+  console.log(itemsData);
 };
 
 // TODO #2.4: Show items in table (Sort itemsData variable based on created_date in ascending order)
-const showItemsInTable = (itemsData) => {
-  const table_body = document.getElementById("main-table-body");
-  table_body.innerHTML = "";
+const showItemsInList = (itemsData) => {
   // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
-
+  itemsData.sort((a, b) => {
+    try{
+      let A = a.due_date.split("-").map(x => parseInt(x));
+      let B = b.due_date.split("-").map(x => parseInt(x));
+      if (A < B) return -1;
+      else if (A > B) return 1;
+      else return 0;
+    }
+    catch {return ;}
+  });
   // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
   itemsData.map((item) => {
     // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
-    table_body.innerHTML += `
-        <tr id="${item.item_id}">
-            <td>${item.item}</td>
-            <td>Name</td>
-            <td>Price</td>
-            <td><button class="delete-row" onclick="deleteItem('${item.item_id}')">ลบ</button></td>
-        </tr>
-        `;
+    addItemtolist(item);
     // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
   });
 };
 
+//myadd
+function addItemtolist(item) {
+  const taskList = document.getElementById('taskList');
+  
+  const oneRow = document.createElement("div");
+  const note = document.createElement("div");
+  const taskItem = document.createElement("button");
+  const bin = document.createElement("button");
+
+  note.className = "note";
+  taskItem.className = "task";
+  bin.className = "deleteButton";
+  let i = taskList.childElementCount;
+  oneRow.id = `oneRow${i}`;
+  oneRow.className = "oneRow";
+  note.id = `note${i}`;
+  taskItem.id = `task${i}`;
+  bin.id = `bin${i}`;
+  bin.innerHTML = `
+                  <img src="./img/image-removebg-preview 1.png" alt="bin">
+                  `;
+  if (item.link != "") {
+    note.innerHTML = `
+                          <div class = "note-text">Note</div>
+                          <div class = "input-note-text"> ${item.note} </div>
+                          <a class = "link-text" href="${item.link}">Link to ${item.title}</a>
+                      `;
+  } else {
+    note.innerHTML = `
+                          <div class = "note-text" style= "height : 120 ";>Note</div>
+                          <div class = "input-note-text"> ${item.note} </div>
+                      `;
+  }
+  taskItem.innerHTML = `
+                      <button class = "check-task" onclick = "checkTask(event,${i})">
+                          <div id = "checkMark${i}" class = "checkMark"></div>
+                      </button>
+                      <span id="taskName${i}" class="taskName">${item.title}</span>
+                      <span id="taskDueDate${i}" class = "taskDueDate">${item.due_date}</span>
+                  `;
+  oneRow.appendChild(taskItem);
+  oneRow.appendChild(bin);
+  taskList.appendChild(oneRow);
+  taskList.appendChild(note);
+  taskItem.addEventListener("click", () => {
+    clickTask(i);
+  });
+  bin.addEventListener("click", () => {
+    deleteTask(item.item_id);
+  });
+};
+
+function checkTask(event,idx){
+  let x = document.getElementById("checkMark"+idx);
+  let y = document.getElementById("taskName"+idx);
+  console.log(x.style.display);
+  event.stopPropagation();
+  if (x.style.display != "flex" || x.style.display == null) {
+      x.style.display = "flex";
+      y.style.textDecoration = "line-through";
+  } else {
+      x.style.display = "none";
+      y.style.textDecoration = "none";
+  }
+}
+function clickTask(idx){
+  let note = document.getElementById("note"+idx);
+  let bin = document.getElementById("bin"+idx);
+  console.log(bin.style.display)
+  if(note.style.display != 'flex' || note.style.display == null) {
+      note.style.display = 'flex';
+      bin.style.visibility = 'visible';
+  } else {
+      note.style.display = 'none';
+      bin.style.visibility = 'hidden';
+  }
+}
+
+function deleteTask(item_id){
+  deleteItem(item_id);
+}
+
 // TODO #2.5: Send Add an item ("POST") request to backend server and update items in the table
 const addItem = async () => {
-  const item = document.getElementById("item-to-add").value;
-  const name = document.getElementById("name-to-add").value;
-  const price = document.getElementById("price-to-add").value;
+  const taskInput = document.getElementById('title');
+  const taskDueDate = document.getElementById('dueDate');
+  const taskNote = document.getElementById('note');
+  const taskLink = document.getElementById('link')
+  const taskCategory = document.getElementById('selectCategory');
+  const newCategory = document.getElementById('addCategory');
+  let cg =  taskCategory.value;
+  if (taskInput.value.trim() === '') return;
+  if (taskDueDate.value.trim() === '') return;
+  if (cg == "other"){
+    cg = newCategory.value;
+  }
+  const itemToAdd = {
+    title: taskInput.value,
+    category: cg,
+    due_date: taskDueDate.value,
+    note: taskNote.value,
+    link: taskLink.value,
+    user_name: "knot"
+  }
+  const options = {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(itemToAdd)
+  }
+  await fetch(`http://${backendIPAddress}/items`, options)
+    .then((response) => {
+      document.getElementById('title').value = "";
+      document.getElementById('dueDate').value = "";
+      document.getElementById('note').value = "";
+      document.getElementById('link').value = "";
+      document.getElementById('selectCategory').value = 0;
+      document.getElementById('addCategory').value = "";
+    })
+    .catch((error) => console.error(error));
 
-  console.log(
-    "This function should fetch 'add item' route from backend server and update items in the table."
-  );
+  await getItemsFromDB();
+  cleartask();
+  showItemsInList(itemsData);
 };
+
+function cleartask(){
+  taskList.innerHTML = "";
+}
 
 // TODO 2.6: Send Delete an item ("DELETE") request to backend server and update items in the table
 const deleteItem = async (item_id) => {
-  console.log(
-    "This function should fetch 'delete item' route in backend server and update items in the table."
-  );
+  const options = {
+    method: "DELETE",
+    credentials: "include"
+  }
+  await fetch(`http://${backendIPAddress}/items/${item_id}`, options)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => console.error(error));
+
+  await getItemsFromDB();
+  cleartask();
+  showItemsInList(itemsData);
 };
 
 const redrawDOM = () => {
@@ -88,17 +227,17 @@ const redrawDOM = () => {
       cancelable: true,
     })
   );
-  document.getElementById("item-to-add").value = "";
-  document.getElementById("name-to-add").value = "0";
-  document.getElementById("price-to-add").value = "";
+  document.getElementById("task-to-add").value = "";
+  document.getElementById("category-to-add").value = 0;
+  document.getElementById("dueDate-to-add").value = "";
 };
 
-document.getElementById("group-no").innerHTML = getGroupNumber();
+//document.getElementById("group-no").innerHTML = getGroupNumber();
 
 document.addEventListener("DOMContentLoaded", async function (event) {
-  console.log("Showing group members.");
-  await showGroupMembers();
+  //console.log("Showing Categories.");
+  //await showCategories();
   console.log("Showing items from database.");
   await getItemsFromDB();
-  showItemsInTable(itemsData);
+  showItemsInList(itemsData);
 });
